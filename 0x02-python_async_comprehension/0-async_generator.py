ff79f2a41bd16bc010ddc9c2489a4683462f5b22
ClintonCMOs <orengecmo@gmail.com>
2024-08-07 00:46:19 +0300

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A module that yields asynchronous values
+"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    An asynchronous function that generates asynchronous values
+    without taking args
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
